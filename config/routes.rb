51f428d3d7aecf68fa92c494b16e79b92f29af5a
mehdi sabil <mehdi.sabil@hotmail.com>
2023-02-24 12:30:06 +0100

    bundle

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'lists', to: 'lists#index'
+  post 'lists', to: 'lists#create'
+  get 'lists/:id', to: 'lists#show'
+  get 'lists/new', to: 'lists#new'
   # Defines the root path route ("/")
   # root "articles#index"
 end
